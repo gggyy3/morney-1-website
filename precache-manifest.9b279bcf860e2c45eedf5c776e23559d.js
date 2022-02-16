f19256c5b64a7bffbea2ed05f753f374f4ffbc66
gggyy3 <1587644910@qq.com>
2022-02-16 16:09:36 +0800

    deploy

---
+++ precache-manifest.9b279bcf860e2c45eedf5c776e23559d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "733f46dac7e072d141b8",
+    "url": "/morney-1-website/css/app.f07439c9.css"
+  },
+  {
+    "revision": "971db398d3333c22a28d025968fd44f6",
+    "url": "/morney-1-website/index.html"
+  },
+  {
+    "revision": "733f46dac7e072d141b8",
+    "url": "/morney-1-website/js/app.31c1f18d.js"
+  },
+  {
+    "revision": "c43cf8354822c3c79b8e",
+    "url": "/morney-1-website/js/chunk-vendors.867c1812.js"
+  },
+  {
+    "revision": "b0c72820654d086f2b526db8c6a20387",
+    "url": "/morney-1-website/manifest.json"
+  },
+  {
+    "revision": "e89e37a3b140af25e577c9be364b756d",
+    "url": "/morney-1-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-1-website/robots.txt"
+  }
+]);
